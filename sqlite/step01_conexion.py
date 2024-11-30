ed1920712b598d57a5515780587da41058724ede
Jimmy Garcia <jjgarcia6@utpl.edu.ec>
2024-11-30 00:25:03 -0500

    conexion a base

--- sqlite/step01_conexion.py
+++ sqlite/step01_conexion.py
@@ -1,8 +1,8 @@
-"""Crear la base de datos de Python con SQLite."""
+"""Crear la base de datos de Python con SQLite"""
 
 import sqlite3
 
-# Ruta de la base de datos
+# Ruta de la base de datos SQLite
 DB_PATH = 'sqlite/baseDB.db'
 
 # Crear una nueva conexión a la base de datos
