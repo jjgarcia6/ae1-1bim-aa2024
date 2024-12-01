c8833d12bf612fc0160950defc7d1c3058f63d78
Jimmy Garcia <jjgarcia6@utpl.edu.ec>
2024-11-30 22:43:47 -0500

    actualizacion insert

--- sqlite/step03_insert.py
+++ sqlite/step03_insert.py
@@ -2,40 +2,40 @@
 
 from step01_conexion import conn
 
-# Datos a insertar para locales de comida
-locales_de_comida = [
-    (1, 'Los Paisas', 'Av. Primera 123 y calle 45', '1234567', 'Colombiana', 50),
-    (2, 'Orale Mano', 'C.C. La Piazza locales # 56-58', '2345678', 'Mexicana', 60),
-    (3, 'Roma', 'Av. San Pedro # 67-89', '3456780', 'Italiana', 70),
-    (4, 'Michi', 'Calle 456 # 78-90', '4567890', 'China', 80),
-    (5, 'No vives de ensalada', 'Los Toritos #555', '5678901', 'Argentina', 90),
-    (6, 'Baratie', 'East Blue #300', '6789012', 'Japonesa', 100)
+# Datos a insertar para clinicas
+clinicas = [
+    (1, 'Los Cuervos', 'Av. Primera 123 y calle 45', '1234567', 'matutina', 50),
+    (2, 'IESS', 'C.C. La Piazza locales # 56-58', '2345678', 'nocturna', 60),
+    (3, 'Cruz Roja', 'Av. San Pedro # 67-89', '3456780', 'nocturna', 70),
+    (4, 'Alcivar', 'Calle 456 # 78-90', '4567890', 'matutina', 80),
+    (5, 'Granados', 'Los Toritos #555', '5678901', 'matutina', 90),
+    (6, 'Baratie', 'East Blue #300', '6789012', 'matutina', 100)
 ]
 
-# Datos a insertar para centros deportivos
-centros_deportivos = [
-    (1, 'Taurus', 'Sauces 8 #547 y peatonal 3era', '1234567', 'Pesas', 55),
-    (2, 'Gym Nova', 'Av. Francisco de Orellana # 123', '2345678', 'Cardio', 110),
-    (3, 'Maori', 'C.C. Alban Borja local #33', '3456780', 'Crossfit', 40),
-    (4, 'HT Gym', 'Villa Club Etapa Neptuno', '4567890', 'Pesas', 30),
-    (5, 'Urdesa Center', 'Victor Emilio Estrada # 700', '5678901', 'Cardio', 80),
-    (6, 'Raza', 'Federacion Central # 934', '6789012', 'Crossfit', 45)
+# Datos a insertar para parques
+parques = [
+    (1, 'Disney', 'Sauces 8 #547 y peatonal 3era', '1234567', 'Guayaquil', 55),
+    (2, 'Malecon', 'Av. Francisco de Orellana # 123', '2345678', 'Quito', 110),
+    (3, 'Milenio', 'C.C. Alban Borja local #33', '3456780', 'Cuenca', 40),
+    (4, 'Asia', 'Villa Club Etapa Neptuno', '4567890', 'Cuenca', 30),
+    (5, 'Historico', 'Victor Emilio Estrada # 700', '5678901', 'Cuenca', 80),
+    (6, 'Natural', 'Federacion Central # 934', '6789012', 'Guayaquil', 45)
 ]
 
 # Definir la sentencia SQL para insertar registros en las tablas
-INSERT_LOCALES = '''
-INSERT INTO locales_de_comida (id, nombre, direccion, telefono, tipo_comida, capacidad)
+INSERT_CLINICAS = '''
+INSERT INTO clinicas (id, nombre, direccion, telefono, jornada, capacidad)
 VALUES (?, ?, ?, ?, ?, ?)
 '''
 
-INSERT_CENTROS = '''
-INSERT INTO centros_deportivos (id, nombre, direccion, telefono, tipo_deporte, capacidad)
+INSERT_PARQUES = '''
+INSERT INTO parques (id, nombre, direccion, telefono, ubicacion, capacidad)
 VALUES (?, ?, ?, ?, ?, ?)
 '''
 
 # Insertar los registros en las tablas
 with conn:
     cursor = conn.cursor()
-    cursor.executemany(INSERT_LOCALES, locales_de_comida)
-    cursor.executemany(INSERT_LOCALES, centros_deportivos)
+    cursor.executemany(INSERT_CLINICAS, clinicas)
+    cursor.executemany(INSERT_PARQUES, parques)
     print('Registros insertados correctamente!!')
