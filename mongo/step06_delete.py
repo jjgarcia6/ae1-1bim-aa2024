b5b5c3ba2ed0bb4e3d2733178ff06134529c2362
Jimmy Garcia <jjgarcia6@utpl.edu.ec>
2024-11-30 23:42:01 -0500

    actualizacion delete

--- mongo/step06_delete.py
+++ mongo/step06_delete.py
@@ -1,25 +1,25 @@
-'''Delete de documentos en colección locales y centros'''
+'''Delete de documentos en colección clinicas y parques'''
 
 # Importar el cliente de pymongo
-from step01_conexion import client
+from conexion import client
 
 # Seleccionar la base de datos
 db = client.documentos
 
 # Seleccionar las colecciones
-coleccion_locales = db.locales
-coleccion_centros = db.centros
+coleccion_clinicas = db.clinicas
+coleccion_parques = db.parques
 
-# Eliminar un documento de la coleccion locales
-borrado_01 = coleccion_locales.delete_one(
+# Eliminar un documento de la coleccion clinicas
+borrado_01 = coleccion_clinicas.delete_one(
     {"nombre": "Baratie"}
 )
 print("\n1) Eliminar un solo documento")
 print(f"Documentos eliminados: {borrado_01.deleted_count}")
 
-# Eliminar multiples documentos de la coleccion centros
-borrado_02 = coleccion_centros.delete_many(
-    {"capacidad": 45}
+# Eliminar multiples documentos de la coleccion parques
+borrado_02 = coleccion_parques.delete_many(
+    {"capacidad": 450}
 )
 print("\n2) Eliminar múltiples documentos")
 print(f"Documentos eliminados: {borrado_02.deleted_count}")
