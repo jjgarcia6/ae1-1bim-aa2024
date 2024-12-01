d8f72df65dbe7f2cea12993841f644cfae60f8ad
Jimmy Garcia <jjgarcia6@utpl.edu.ec>
2024-11-30 23:42:45 -0500

    actualizacion update

--- mongo/step05_update.py
+++ mongo/step05_update.py
@@ -1,47 +1,47 @@
-'''Actualización de documentos en colección locales y centros'''
+'''Actualización de documentos en colección clinicas y parques'''
 
 # Importar el cliente de pymongo
-from step01_conexion import client
+from conexion import client
 
 # Seleccionar la base de datos
 db = client.documentos
 
 # Seleccionar las colecciones
-coleccion_locales = db.locales
-coleccion_centros = db.centros
+coleccion_clinicas = db.clinicas
+coleccion_parques = db.parques
 
-# Actualizar un documento de la colección locales
-actualizacion01 = coleccion_locales.update_one(
-    {"nombre": "Michi"},
+# Actualizar un documento de la colección clinicas
+actualizacion01 = coleccion_clinicas.update_one(
+    {"nombre": "Michi Veterinaria"},
     {"$set": {"estado": "Abierto"}}
 )
-print("\n1) Actualizar un solo documento en coleccion locales")
+print("\n1) Actualizar un solo documento en coleccion clinicas")
 print(f"Documentos coincidentes: {actualizacion01.matched_count}")
 print(f"Documentos modificados: {actualizacion01.modified_count}")
 
-# Actualizar varios documentos de la colección locales
-actualizacion02 = coleccion_locales.update_many(
+# Actualizar varios documentos de la colección clinicas
+actualizacion02 = coleccion_clinicas.update_many(
     {"capacidad": {"$gte": 50}},
     {"$set": {"estado": "Abierto"}}
 )
-print("\n2) Actualizar múltiples documentos en coleccion locales")
+print("\n2) Actualizar múltiples documentos en coleccion clinicas")
 print(f"Documentos coincidentes: {actualizacion02.matched_count}")
 print(f"Documentos modificados: {actualizacion02.modified_count}")
 
-# Actualizar un documento de la colección centros
-actualizacion03 = coleccion_centros.update_one(
-    {"nombre": "Gym Nova"},
-    {"$set": {"capacidad": 50}}
+# Actualizar un documento de la colección parques
+actualizacion03 = coleccion_parques.update_one(
+    {"nombre": "Nova Park"},
+    {"$set": {"capacidad": 700}}
 )
-print("\n3) Actualizar un solo documento en coleccion centros")
+print("\n3) Actualizar un solo documento en coleccion parques")
 print(f"Documentos coincidentes: {actualizacion03.matched_count}")
 print(f"Documentos modificados: {actualizacion03.modified_count}")
 
-# Actualizar varios documentos de la colección centros
-actualizacion04 = coleccion_centros.update_many(
-    {"capacidad": {"$gte": 45}},
+# Actualizar varios documentos de la colección parques
+actualizacion04 = coleccion_parques.update_many(
+    {"capacidad": {"$gte": 450}},
     {"$set": {"instructor": "Zoro"}}
 )
-print("\n4) Actualizar múltiples documentos en coleccion centros")
+print("\n4) Actualizar múltiples documentos en coleccion parques")
 print(f"Documentos coincidentes: {actualizacion04.matched_count}")
 print(f"Documentos modificados: {actualizacion04.modified_count}")
